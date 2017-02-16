917dbd9db8978fa27eaa6873430737ef9f710723
Andrew Caulfield <andrewjcaulfield@gmail.com>
2017-02-16 23:46:31 +0000

    Removed package.json and went to CDN

--- observable/index.js
+++ observable/index.js
@@ -1,16 +1,27 @@
 /*
 Observables can be used to model events, asynchronous requests, and animations. Observables can also be transformed, combined, and consumed using the Array methods
+
+We must forEach over the Observable for it to work.
 */
-
-var Rx = require('rxjs');
-
 var Observable = Rx.Observable;
 
 var button = document.getElementById('button');
 
 var clicks = Observable.fromEvent(button, 'click');
 
+var points =
+    clicks.map(function(e) {
+        return {x: e.clientX, y: e.clientY };
+    });
+
 var subscription =
-    clicks.forEach(function onNext(e) {
-        alert('clicked');
+    points.forEach(function onNext(point) {
+        alert('clicked: ' + JSON.stringify(point));
+        subscription.dispose();
+    },
+    function onError(e) {
+        console.log(e);
+    },
+    function onCompleted() {
+        console.log('Done');
     });
